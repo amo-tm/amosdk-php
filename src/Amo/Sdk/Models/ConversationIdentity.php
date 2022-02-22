a317f9eaa4809f7fde03ba1c2a43014f0950a37b
meremin <meremin@team.amocrm.com>
2022-02-22 14:41:37 +0300

    support direct && channel conversations

--- src/Amo/Sdk/Models/ConversationIdentity.php
+++ src/Amo/Sdk/Models/ConversationIdentity.php
@@ -5,10 +5,24 @@ namespace Amo\Sdk\Models;
 class ConversationIdentity extends AbstractModel
 {
     protected ?string $subjectId = null;
+    protected ?string $channelId = null;
+    protected ?string $directId = null;
 
     static public function subject(string $subjectId): ConversationIdentity {
         return new ConversationIdentity([
             'subject_id' => $subjectId
+        ]);
+    }
+
+    static public function channel(string $channelId): ConversationIdentity {
+        return new ConversationIdentity([
+            'channel_id' => $channelId
+        ]);
+    }
+
+    static public function direct(string $userId): ConversationIdentity {
+        return new ConversationIdentity([
+            'direct_id' => $userId
         ]);
     }
 }
