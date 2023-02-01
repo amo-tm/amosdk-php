04202bf61e790bf2b9fd46bc0095498e7c6298d4
dmaksimenko <dmaksimenko@team.amocrm.com>
2023-02-01 18:32:41 +0300

    changes and return php7.4 v

--- src/Amo/Sdk/Models/Webhook/WebhookRpaRequestUpdated.php
+++ src/Amo/Sdk/Models/Webhook/WebhookRpaRequestUpdated.php
@@ -16,7 +16,7 @@ class WebhookRpaRequestUpdated extends AbstractModel implements Interfaces\Webho
 
     protected string $requestId;
     protected \DateTimeInterface $modifiedAt;
-    protected RequestChangesCollection|array $changes = [];
+    protected ?RequestChangesCollection $changes = null;
 
     protected array $cast = [
         'changes' => RequestChangesCollection::class,
@@ -46,9 +46,9 @@ class WebhookRpaRequestUpdated extends AbstractModel implements Interfaces\Webho
     /**
      * @return RequestChangesCollection|RequestChange[]
      */
-    public function getChanges(): RequestChangesCollection|array
+    public function getChanges(): RequestChangesCollection
     {
-        return $this->changes;
+        return $this->changes ?? new RequestChangesCollection([]);
     }
 
 
